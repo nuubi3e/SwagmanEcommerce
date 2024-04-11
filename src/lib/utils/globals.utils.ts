eda78df64c81c9d1c1fd6c0ccfb4309e1ae076ad
Gaurav <nuubi3e@gmail.com>
2024-04-11 14:56:31 +0530

    added capitalize js function

--- src/lib/utils/globals.utils.ts
+++ src/lib/utils/globals.utils.ts
@@ -2,5 +2,5 @@ export const capitalize = (str: string) =>
   str
     .trim()
     .split(' ')
-    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(' '); // function to capitalize string in JS
